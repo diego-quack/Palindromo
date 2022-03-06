8e5c6ca1a48d019c66bcb70c1942ecf03bc85b7f
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-03-06 13:38:11 -0300

    Teste do método palíndromo ok - retornando true / false.

--- src/application/Palindromo.java
+++ src/application/Palindromo.java
@@ -1,17 +1,26 @@
 package application;
 
+import java.util.Scanner;
+
 public class Palindromo {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Entre com uma palavra pra saber se é um palíndromo ou não: ");
+		String palavra = input.next();
+		System.out.println(palindromo(palavra));
+		
+		input.close();
 
 	}
 	
-	public static boolean palindrome(String palavra) {
+	public static boolean palindromo(String palavra) {
 		
 		int tamanho = palavra.length();
 		
 		for(int i = 0; i < tamanho / 2; i++) {
-			if(palavra.charAt(i) != palavra.charAt(tamanho - i - 1))) {
+			if(palavra.charAt(i) != palavra.charAt(tamanho - i - 1)) {
 				return false;
 			}
 		}
