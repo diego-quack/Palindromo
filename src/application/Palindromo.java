00f544ca5f1bb70ea0dc6b55417f95270dcbc474
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-03-06 14:04:34 -0300

    Mudança no método palíndromo - retornando uma string informandose é palíndromo ou não.

--- src/application/Palindromo.java
+++ src/application/Palindromo.java
@@ -15,16 +15,19 @@ public class Palindromo {
 
 	}
 	
-	public static boolean palindromo(String palavra) {
+	public static String palindromo(String palavra) {
+		
+		String palindromo = "É um palídromo";
+		String naoPalindromo = "Não é um palíndromo.";
 		
 		int tamanho = palavra.length();
 		
 		for(int i = 0; i < tamanho / 2; i++) {
 			if(palavra.charAt(i) != palavra.charAt(tamanho - i - 1)) {
-				return false;
+				return naoPalindromo;
 			}
 		}
-		return true;
+		return palindromo;
 	}
 
 }
