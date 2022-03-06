e4550499296d0aa803908f054ea7fb440d01bad8
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-03-06 13:29:47 -0300

    Método palindromo criado

--- src/application/Palindromo.java
+++ src/application/Palindromo.java
@@ -3,8 +3,19 @@ package application;
 public class Palindromo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	}
+	
+	public static boolean palindrome(String palavra) {
+		
+		int tamanho = palavra.length();
+		
+		for(int i = 0; i < tamanho / 2; i++) {
+			if(palavra.charAt(i) != palavra.charAt(tamanho - i - 1))) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
